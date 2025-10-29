cfe1f4a3b55c6ca11a4a8e9728a0866db78e1d55
Maddy Guthridge <hello@maddyguthridge.com>
2025-10-29 23:39:30 +1100

    Fix potential data loading race condition

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,3 +1,5 @@
+import AsyncLock from 'async-lock';
+
 import { getConfig, type Config } from './config';
 import type { MediaLibrary } from './scanner/types';
 import { findPresets, type PresetInfo } from './handbrakePresets';
@@ -6,6 +8,9 @@ import * as log from './log';
 
 export type Status = 'LOAD_CONFIG' | 'SCAN_MEDIA' | 'READY';
 
+// There is a potential race condition if data loading is triggered twice, and so we use this lock
+// to synchronize calls to `loadData`.
+const lock = new AsyncLock();
 let data: { config: Config, presets: PresetInfo[], media: MediaLibrary[] } | undefined = undefined;
 
 /**
@@ -16,32 +21,41 @@ let data: { config: Config, presets: PresetInfo[], media: MediaLibrary[] } | und
  * 3. Scan media libraries
  */
 export async function loadData() {
-  log.write('Begin data load');
-  // Delete previous data
-  data = undefined;
+  await lock.acquire('data', async () => {
+    log.write('Begin data load');
+    // Delete previous data
+    data = undefined;
 
-  const config = await getConfig();
-  const presets = await findPresets(config.handbrake.presets);
-  const media = await Promise.all(
-    config.libraries.map((library, id) =>
-      scanLibrary(
-        id,
-        library.name,
-        library.staging,
-        library.production,
-        presets,
+    const config = await getConfig();
+    const presets = await findPresets(config.handbrake.presets);
+    const media = await Promise.all(
+      config.libraries.map((library, id) =>
+        scanLibrary(
+          id,
+          library.name,
+          library.staging,
+          library.production,
+          presets,
+        ),
       ),
-    ),
-  );
-  data = { config, presets, media };
-  log.write('All media libraries loaded');
-  return data;
+    );
+    data = { config, presets, media };
+    log.write('All media libraries loaded');
+  });
+  return data!;
 }
 
 /**
  * Return all the server's data
  */
 export async function getData() {
-  // FIXME: potential race condition if data loading is triggered twice
-  return data ?? await loadData();
+  // Wait for data to be unlocked before getting the value
+  return await lock.acquire('data', () => {
+    if (data === undefined) {
+      // Locks are reentrant in the same domain so this will not cause a deadlock (hopefully)
+      return loadData();
+    } else {
+      return data;
+    }
+  });
 }
