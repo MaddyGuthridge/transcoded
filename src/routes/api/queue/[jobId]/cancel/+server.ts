713ce106a5756b1468b79bf2d4952d3c8f1445a3
Maddy Guthridge <hello@maddyguthridge.com>
2025-10-30 14:27:37 +1100

    Prevent cancellation of already completed jobs

--- src/routes/api/queue/[jobId]/cancel/+server.ts
+++ src/routes/api/queue/[jobId]/cancel/+server.ts
@@ -6,9 +6,15 @@ import * as z from 'zod';
 
 export function DELETE(req: RequestEvent) {
   const jobId = zodParse(z.int(), parseInt(req.params.jobId));
-  if (!queue.getQueue().find(job => job.id === jobId)) {
+  const job = queue.getQueue().find(job => job.id === jobId);
+  if (!job) {
     error(400, 'Invalid job ID');
   }
+
+  if (['Complete', 'Failed', 'Canceled'].includes(job.status)) {
+    error(400, 'Cannot cancel an already resolved job');
+  }
+
   queue.cancel(jobId);
   return json(queue.getQueue());
 }
